c7aeedc3ed81a917eeb117a58505ce3db2c47cab
Ibrahim El Khansa <itzbahb@gmail.com>
2025-07-28 13:07:59 +0300

    updated favicon information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,17 @@ export const metadata: Metadata = {
   authors: [{ name: "Ibrahim El-Khansa", url: "https://ibrahimelkhansa.com" }],
   creator: "Ibrahim El Khansa",
   publisher: "Ibrahim El Khansa",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any", type: "image/x-icon" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
+    ],
+    shortcut: "/favicon.ico",
+  },
   robots: {
     index: true,
     follow: true,
